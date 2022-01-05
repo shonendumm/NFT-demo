5c3ad95a5780ea437cd62f351b2dcc7f634cc169
Soo Hian <shonendumm@gmail.com>
2022-01-05 22:01:16 +0800

    add mint nft

--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -6,8 +6,21 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract SimpleCollectible is ERC721 {
 
-    constructor () public ERC721 ("Doggie", "DOG"){
+    uint256 public tokenCounter;
 
+    constructor () public ERC721 ("Doggie", "DOG"){
+        // initialize tokenCounter to 0
+        tokenCounter = 0;
     }
-    
+
+
+    function createCollectible() public returns (uint256) {
+        // use tokenCounter as an id for each created token
+        uint256 newTokenId = tokenCounter;
+        // use _safeMint inherited from ERC721 contract to mint a token
+        _safeMint(msg.sender, newTokenId); 
+        tokenCounter = tokenCounter + 1;
+        return newTokenId;
+    }
+
 }
