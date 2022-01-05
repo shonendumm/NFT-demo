7f99d3b10d736fff55f0e80a55b6d43b647bdc32
Soo Hian <shonendumm@gmail.com>
2022-01-05 21:54:59 +0800

    add constructor and pug image

--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -3,3 +3,11 @@
 pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SimpleCollectible is ERC721 {
+
+    constructor () public ERC721 ("Doggie", "DOG"){
+
+    }
+    
+}
