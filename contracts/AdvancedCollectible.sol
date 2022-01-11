a1822d7a11a0c76527adca866865ba70af4f4eed
Soo Hian <shonendumm@gmail.com>
2022-01-11 21:34:00 +0800

    add comments

--- contracts/AdvancedCollectible.sol
+++ contracts/AdvancedCollectible.sol
@@ -11,19 +11,21 @@ import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 contract AdvancedCollectible is ERC721, VRFConsumerBase {
 
     uint256 public tokenCounter;
+    // keyhash and fee for calling the VRFCoordinator requestRandomness(); we will pass in their values when deploying from brownie/python
+    // make them global variables so that the method can get them (line 38)
     bytes32 public keyhash;
     uint256 public fee;
     enum Breed{PUG, SHIBA_INU, ST_BERNARD}
     mapping(uint256 => Breed) public tokenIdToBreed;
     mapping(bytes32 => address) public requestIdToSender;
     // as a best practice, we emit an event whenever the mapping is updated:
-    // indexed keyword makes it easier to search for this event
+    // indexed keyword makes it easier to search for the requestId / tokenId
     event requestedCollectible(bytes32 indexed requestId, address requester);
     event breedAssigned(uint256 indexed tokenId, Breed breed);
 
 
     // refer to chainlink docs for VRFCoordinator's parameters
-    constructor (address _vrfCoordinator, address _linkToken, bytes32 _keyhash, uint256 _fee) public VRFConsumerBase(_vrfCoordinator, _linkToken) ERC721 ("Dogie", "DOG") 
+    constructor (address _vrfCoordinator, address _linkToken, bytes32 _keyhash, uint256 _fee) public VRFConsumerBase(_vrfCoordinator, _linkToken) ERC721("Dogie", "DOG") 
     {
         tokenCounter = 0;
         keyhash = _keyhash;
@@ -34,6 +36,7 @@ contract AdvancedCollectible is ERC721, VRFConsumerBase {
     function createCollectible() public returns (bytes32){
         // we want the request id 
         bytes32 requestId = requestRandomness(keyhash, fee);
+        // map requestId to sender's address, so that later we have the requestId, can find the owner to assign the NFT
         requestIdToSender[requestId] = msg.sender;
         emit requestedCollectible(requestId, msg.sender);
     }
@@ -42,6 +45,7 @@ contract AdvancedCollectible is ERC721, VRFConsumerBase {
     function fulfillRandomness(bytes32 requestId, uint256 randomNumber) internal override {
         Breed breed = Breed(randomNumber % 3);
         uint256 newTokenId = tokenCounter;
+        // map tokenId to breed
         tokenIdToBreed[newTokenId] = breed;
         emit breedAssigned(newTokenId, breed);
 
@@ -53,7 +57,7 @@ contract AdvancedCollectible is ERC721, VRFConsumerBase {
         tokenCounter = tokenCounter + 1;
     }
 
-    // create a setTokenURI function so that we can call this from outside with the _tokenURI argument
+    // create a setTokenURI function so that we can call this from outside/brownie with the _tokenURI argument
     function setTokenURI(uint256 tokenId, string memory _tokenURI) public {
         // need 3 tokenURIs for the 3 breeds
         // we want to let only the owner of the tokenId set the tokenURI
