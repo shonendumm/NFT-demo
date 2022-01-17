062806d4bba45d718eb400ca617f687b0246f5b2
Soo Hian <shonendumm@gmail.com>
2022-01-17 23:12:14 +0800

    format NFT metadata

--- scripts/advancedCollectible/create_metadata.py
+++ scripts/advancedCollectible/create_metadata.py
@@ -1,11 +1,14 @@
 from brownie import AdvancedCollectible, network
 from scripts.helpful_scripts import getBreed
 from metadata.sample_metadata import metadata_template
+from pathlib import Path
 
-# until https://youtu.be/M576WGiDBdQ?t=40287
+# until https://youtu.be/M576WGiDBdQ?t=40608
+# download ipfs commandline in order to upload images to ipfs programmatically
 
 def main():
     advanced_collectible = AdvancedCollectible[-1]
+    print(f"Your contract is deployed at: {advanced_collectible.address}")
     number_of_advanced_collectibles = advanced_collectible.tokenCounter()
     print(f"You have created {number_of_advanced_collectibles} collectibles!")
     for token_id in range(number_of_advanced_collectibles):
@@ -13,7 +16,20 @@ def main():
         breed = getBreed(advanced_collectible.tokenIdToBreed(token_id))
         metadata_file_name = f"./metadata/{network.show_active()}/{token_id}-{breed}.json"
         print(metadata_file_name)
+        collectible_metadata = metadata_template
+        
+        if Path(metadata_file_name).exists():
+            print(f"{metadata_file_name} already exists! Delete it to overwrite")
+        else:
+            print(f"Creating Metadata file: {metadata_file_name}")
+            collectible_metadata["name"] = breed
+            collectible_metadata["description"] = f"An adorable {breed}"
+            print(collectible_metadata)
+            image_path = "./img/" + breed.lower().replace("_", "-") + ".png"
+            # image_uri = upload(image_path)
+            # collectible_metadata["image"] = image_uri
 
-
-
-
+def upload_to_IPFS(filepath):
+    with Path(filepath).open("rb") as fp:
+        image_binary = fp.read()
+        # upload via IPFS commandline
