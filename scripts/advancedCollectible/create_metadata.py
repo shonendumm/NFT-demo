500dc44acb0328ed45975ce335329ed498df2c08
Soo Hian <shonendumm@gmail.com>
2022-01-18 20:56:21 +0800

    add upload image to IPFS

--- scripts/advancedCollectible/create_metadata.py
+++ scripts/advancedCollectible/create_metadata.py
@@ -2,6 +2,7 @@ from brownie import AdvancedCollectible, network
 from scripts.helpful_scripts import getBreed
 from metadata.sample_metadata import metadata_template
 from pathlib import Path
+import requests
 
 # until https://youtu.be/M576WGiDBdQ?t=40608
 # download ipfs commandline in order to upload images to ipfs programmatically
@@ -25,11 +26,26 @@ def main():
             collectible_metadata["name"] = breed
             collectible_metadata["description"] = f"An adorable {breed}"
             print(collectible_metadata)
+            # formatting the image_path so that we can get the respective files
             image_path = "./img/" + breed.lower().replace("_", "-") + ".png"
-            # image_uri = upload(image_path)
+            image_uri = upload_to_IPFS(image_path)
             # collectible_metadata["image"] = image_uri
 
 def upload_to_IPFS(filepath):
+    # open the image files as read binary
     with Path(filepath).open("rb") as fp:
         image_binary = fp.read()
         # upload via IPFS commandline
+        # to our own IPFS node, via http api; can check IPFS documentation
+        ipfs_url = "http://127.0.0.1:5001"
+        # make a post request to its endpoint 
+        endpoint = "/api/v0/add"
+        response = requests.post(ipfs_url + endpoint, files={"file":image_binary})
+        ipfs_hash = response.json()["Hash"]
+        # "./img/PUG.png" -> "PUG.pug" (We split the string by / into an array, then take the last item)
+        filename = filepath.split("/")[-1:][0]
+        # this is the image_uri path where the image is stored
+        image_uri = f"https://ipfs.io/ipfs/{ipfs_hash}?filename={filename}"
+        print(image_uri)
+        return image_uri
+
